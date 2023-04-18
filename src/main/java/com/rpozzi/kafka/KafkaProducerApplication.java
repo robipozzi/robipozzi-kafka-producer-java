b591b03f9c87a8f80dc176561f8cdd8251ce9903
Roberto Pozzi <r.robipozzi@gmail.com>
2023-04-18 18:29:52 +0200

    initialize

---
+++ src/main/java/com/rpozzi/kafka/KafkaProducerApplication.java
@@ -0,0 +1,33 @@
+package com.rpozzi.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerApplication.class, args);
+	}
+	
+	@Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("temperatures")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send("temperatures", "test");
+        };
+    }
+
+}
---
+++ src/main/java/com/rpozzi/kafka/dto/Sensor.java
@@ -0,0 +1,33 @@
+package com.rpozzi.kafka.dto;
+
+public class Sensor {
+	private int temperature;
+	private int humidity;
+
+	public Sensor() {
+		super();
+	}
+
+	public Sensor(int temperature, int humidity) {
+		super();
+		this.temperature = temperature;
+		this.humidity = humidity;
+	}
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
+
+	public int getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+
+}
